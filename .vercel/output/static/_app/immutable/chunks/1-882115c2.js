2882368aa3c7f214b21b5b587d59174971ef2175
mgrin212 <matthew_grinberg@brown.edu>
2022-12-23 15:56:10 -0500

    assist bug fixed

--- .vercel/output/static/_app/immutable/chunks/1-882115c2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-5cd9e4f7.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/2-e0d19f07.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-70ef388f.js";export{t as component};
