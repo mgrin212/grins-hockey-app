fdbb73b468b185bf0427decd088a5756493b61a4
mgrin212 <matthew_grinberg@brown.edu>
2022-12-31 17:17:42 -0500

    standings

--- src/routes/standings/+page.svelte
+++ src/routes/standings/+page.svelte
@@ -41,24 +41,6 @@
         {/each}
       {/each}
     {/if}
-    {#if leagueStandings}
-      <div class=" w-full flex flex-row justify-between bg-gray-700">
-        <div class="text-2xl  w-[200px] ml-1">
-          {leagueStandings.league}
-        </div>
-        <div
-          class="flex flex-row justify-between w-[190px] text-center items-center align-middle"
-        >
-          <div class="w-[40px]">GP</div>
-          <div class="w-[40px]">W</div>
-          <div class="w-[40px]">L</div>
-          <div class="w-[40px]">OT</div>
-          <div class="w-[40px]">PTS</div>
-        </div>
-      </div>
-      {#each leagueStandings.standings as team}
-        <StandingsRow props={team} />
-      {/each}
-    {/if}
+    
   </div>
 </div>
